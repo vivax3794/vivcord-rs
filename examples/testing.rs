2c8ec85b236bf45cd5338073ade680ac97add8ee
vivax3794 <vivax3794@protonmail.com>
2022-09-11 02:57:04 +0200

    update example

--- examples/testing.rs
+++ examples/testing.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 const TOKEN: &str = include_str!("../token.secret");
 
 #[tokio::main]
@@ -5,11 +7,29 @@ async fn main() {
     let api = vivcord::ApiClient::new(TOKEN);
     let url = api.get_gateway_url().await.unwrap();
 
+    let intents = vivcord::Intents::GUILD_MESSAGES | vivcord::Intents::MESSAGE_CONTENT;
 
     let mut gateway = vivcord::Gateway::new();
-    gateway.connect(&url, TOKEN).await;
-    gateway.on(|event| async move {
-        println!("CALLBACK TEST: {event:?}")
-    }).await;
-    
+    gateway.connect(&url, TOKEN, &intents).await;
+    gateway
+        .on(
+            Arc::new(tokio::sync::Mutex::new(api)),
+            |event, api| async move {
+                if let vivcord::GatewayEventData::MessageCreate(msg) = event {
+                    if msg.content == "test" {
+                        api.lock()
+                            .await
+                            .create_message(
+                                msg.channel_id,
+                                vivcord::CreateMessageParams {
+                                    content: Some("hello there!".to_owned()),
+                                },
+                            )
+                            .await
+                            .unwrap();
+                    }
+                }
+            },
+        )
+        .await;
 }
